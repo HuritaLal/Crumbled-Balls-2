506952dc82eaea2c6016abb0395266d084f52260
HuritaLal <69113689+HuritaLal@users.noreply.github.com>
2020-11-27 11:55:34 +0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var dustbin,paper,ground;	
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	dustbin = new Dustbin(1200,650);
+	paper = new Paper(200,450,70);
+	ground = new Ground(width/2,670,width,20);
+
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+  dustbin.display();
+  paper.display();
+  ground.display();
+
+  Engine.run(engine);
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paper.body,paper.body.position,{x:165,y:-120});
+       //Body.applyForce(paper.body,paper.body.position,{x:85,y:-85})
+  	}
+}
+
+
